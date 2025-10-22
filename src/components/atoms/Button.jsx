c44d229a1620136883d9c1d00f8b6a3e644bcee6
Santiago Yate <santiyate7@gmail.com>
2025-10-22 00:14:04 -0500

    Load the atoms

---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,17 @@
+export default function Button({
+  color = "bg-indigo-600",
+  children,
+  onClick,
+  type = "button",
+  className,
+}) {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`${className} ${color} text-white font-semibold py-3 px-8 rounded-lg transition cursor-pointer`}
+    >
+      {children}
+    </button>
+  );
+}
---
+++ src/components/atoms/Input.jsx
@@ -0,0 +1,19 @@
+export default function Input({
+  type = "text",
+  value,
+  onChange,
+  placeholder = "Label",
+  required = false,
+  className,
+}) {
+  return (
+    <input
+      type={type}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      required={required}
+      className={`${className} p-3 rounded-lg bg-gray-700 text-white focus:ring-2 focus:ring-indigo-500 outline-none w-full`}
+    />
+  );
+}
