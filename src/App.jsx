637af237c0e6656eb5cfa2627c2a299147aac9e0
Santiago Yate <santiyate7@gmail.com>
2025-10-21 23:37:23 -0500

    Building the chat framework

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 const App = () => {
-  return <h1>Hola mundo</h1>;
+  return (
+    <div className="flex flex-col h-screen">
+      {/* Parte superior - chat */}
+      <div className="p-5 bg-red-500"></div>
+      {/* Conversacion - chat */}
+      <div className="flex-1 bg-green-500"></div>
+      {/* Parte inferior - chat */}
+      <div className="p-5 bg-blue-500"></div>
+    </div>
+  );
 };
 
 export default App;
