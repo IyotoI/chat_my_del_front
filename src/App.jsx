84834f36b5ff81b13c724bcaf08cfacb0a272dbd
Santiago Yate <santiyate7@gmail.com>
2025-10-22 22:58:21 -0500

    Create validations on the cards so that they change depending on the user

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,11 @@ const App = () => {
       {/* Conversacion - chat */}
       <div className="flex-1 bg-green-500 px-2">
         {/* Tarjeta mensaje */}
-        <CardMessages />
+        <CardMessages className="mb-1 bg-[#242626] mr-20 sm:mr-60" />
+        <CardMessages className="mb-1 bg-[#144D37] ml-20 sm:ml-60" />
+        <CardMessages className="mb-1 bg-[#242626] mr-20 sm:mr-60" />
+        <CardMessages className="mb-1 bg-[#144D37] ml-20 sm:ml-60" />
+        <CardMessages className="mb-1 bg-[#242626] mr-20 sm:mr-60" />
       </div>
       {/* Parte inferior - chat */}
       <div className="p-5 bg-blue-500">
--- src/components/molecules/CardMessages.jsx
+++ src/components/molecules/CardMessages.jsx
@@ -1,11 +1,13 @@
-export default function CardMessages() {
+export default function CardMessages({ className }) {
   return (
-    <div className="bg-amber-500 rounded-md break-all p-2 text-base/5 mr-20 sm:mr-60   inline-block">
+    <div
+      className={`${className} rounded-md break-all p-2 text-base/5 inline-block text-white`}
+    >
       <div>
         aaaaadddddddddddddddddddddddddddddddddddaaaaaaaaaaaaaaaaaaaaaaaaaaa
         dddddddddddddddddddddddddddddddddddd
       </div>
-      <div className="text-end">9:27 p.m.</div>
+      <div className="text-end text-[#A2A2A2]">9:27 p.m.</div>
     </div>
   );
 }
