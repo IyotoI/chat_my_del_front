66d8dc80f8bbb869cdcb255811a4037534c0b9ed
Santiago Yate <santiyate7@gmail.com>
2025-10-22 22:42:26 -0500

    Create message card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import BottomChat from "./components/molecules/bottomChat";
+import CardMessages from "./components/molecules/CardMessages";
 
 const App = () => {
   return (
@@ -6,7 +7,10 @@ const App = () => {
       {/* Parte superior - chat */}
       <div className="p-5 bg-red-500"></div>
       {/* Conversacion - chat */}
-      <div className="flex-1 bg-green-500"></div>
+      <div className="flex-1 bg-green-500 px-2">
+        {/* Tarjeta mensaje */}
+        <CardMessages />
+      </div>
       {/* Parte inferior - chat */}
       <div className="p-5 bg-blue-500">
         <BottomChat />
---
+++ src/components/molecules/CardMessages.jsx
@@ -0,0 +1,11 @@
+export default function CardMessages() {
+  return (
+    <div className="bg-amber-500 rounded-md break-all p-2 text-base/5 mr-20 sm:mr-60   inline-block">
+      <div>
+        aaaaadddddddddddddddddddddddddddddddddddaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        dddddddddddddddddddddddddddddddddddd
+      </div>
+      <div className="text-end">9:27 p.m.</div>
+    </div>
+  );
+}
