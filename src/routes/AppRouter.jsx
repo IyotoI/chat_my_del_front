7cc5378389892cac67cb8ae2b1dd216b3b33393f
Santiago Yate <santiyate7@gmail.com>
2025-10-26 19:15:16 -0500

    Test route

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,19 +1,15 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ChatPage from "../components/pages/ChatPage";
 
 export default function AppRouter() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
+        <Route path="/" element={<h1>Hola</h1>} />
         <Route path="/chat" element={<ChatPage />} />
         {/* Redirección por defecto */}
         {/* <Route path="*" element={<Navigate to="/chat" replace />} /> */}
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
