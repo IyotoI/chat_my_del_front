381e2907ac71824cbbc35884cbe2a53ae4bba4d7
Santiago Yate <santiyate7@gmail.com>
2025-10-28 22:53:36 -0500

    Create form login

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,9 +1,24 @@
+import Button from "../atoms/Button";
+import Input from "../atoms/Input";
 import LoginTemplate from "../templates/LoginTemplate";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate("/chat");
+  };
+
   return (
     <LoginTemplate>
-      <h1>Login formulario</h1>
+      <form className="text-center">
+        <Input
+          className="bg-white px-5 mr-2 text-black shadow-[0px_3px_8px_-4px_#777777] mb-8"
+          placeholder="Llave"
+        />
+        <Button onClick={handleLogin}>Crear sala</Button>
+      </form>
     </LoginTemplate>
   );
 }
