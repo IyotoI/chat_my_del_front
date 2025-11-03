fecc4ba0035c81158354eb0b55c3bebc1adcab7b
Santiago Yate <santiyate7@gmail.com>
2025-11-03 03:40:22 -0500

    Add an invitation text for the login screen

--- src/components/molecules/form/FormLogin.jsx
+++ src/components/molecules/form/FormLogin.jsx
@@ -13,9 +13,16 @@ const FormLogin = () => {
   return (
     <form className="text-center">
       <img src={imgOnlineCommunity} alt="Logo" />
-      <p class="text-4xl mb-8 mt-6 font-[Arial]">
+
+      <p class="text-4xl mt-8 mb-4 font-[Arial]">
         <span className="text-[#1AAD5E]">C</span>hatmyd
       </p>
+
+      <p className="text-[#1AAD5E] bg-[#D9FCD2] p-2 text-base/5 mb-6 rounded-2xl ">
+        Crea una contraseña y compartela con tus amigos para que puedan
+        comunicarse
+      </p>
+
       <Input placeholder="Llave" className="mb-8" />
       <Button color="bg-[#1AAD5E]" onClick={handleLogin}>
         Crear sala
