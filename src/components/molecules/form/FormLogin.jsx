9c6facba85fc130db3fefd53dadd94403264bae8
Santiago Yate <santiyate7@gmail.com>
2025-11-03 03:25:56 -0500

    Add the app name to the login screen

--- src/components/molecules/form/FormLogin.jsx
+++ src/components/molecules/form/FormLogin.jsx
@@ -12,7 +12,10 @@ const FormLogin = () => {
 
   return (
     <form className="text-center">
-      <img src={imgOnlineCommunity} alt="Logo" className="mb-15" />
+      <img src={imgOnlineCommunity} alt="Logo" />
+      <p class="text-4xl mb-8 mt-6 font-[Arial]">
+        <span className="text-[#1AAD5E]">C</span>hatmyd
+      </p>
       <Input placeholder="Llave" className="mb-8" />
       <Button color="bg-[#1AAD5E]" onClick={handleLogin}>
         Crear sala
