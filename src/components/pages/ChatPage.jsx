97e85d3ad0074403b16c29ae39c7f0e9bad7e1ae
Santiago Yate <santiyate7@gmail.com>
2025-11-07 06:28:02 -0500

    Integrate listening sockets into the chat page

--- src/components/pages/ChatPage.jsx
+++ src/components/pages/ChatPage.jsx
@@ -1,10 +1,31 @@
 import ChatOrganism from "../organisms/ChatOrganism";
 import ChatTemplate from "../templates/ChatTemplate";
 import { useSocket } from "../../hooks/useSocket";
+import { useEffect } from "react";
 
 export default function ChatPage() {
   const socket = useSocket();
   console.log(socket);
+
+  useEffect(() => {
+    if (!socket) return;
+
+    socket.on("chat:idSocket", (idSocket) => {
+      console.log("🚀 ~ ChatPage ~ idSocket:", idSocket);
+    });
+
+    socket.on("chat:message", (value) => {
+      console.log("🚀 ~ ChatPage ~ value:", value);
+    });
+
+    socket.on("join-room", (value) => {
+      console.log("🚀 ~ ChatPage ~ value:", value);
+    });
+
+    socket.on("chat:fieldWriting", (value) => {
+      console.log("🚀 ~ ChatPage ~ value:", value);
+    });
+  }, []);
 
   return (
     <ChatTemplate>
