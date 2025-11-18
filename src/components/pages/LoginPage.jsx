b5a406a928eed108282b2f84096fe7becbbd98c4
Santiago Yate <santiyate7@gmail.com>
2025-11-18 00:01:12 -0500

    Register the room with the socket

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSocket } from "../../hooks/useSocket";
 import FormLogin from "../molecules/form/FormLogin";
 import LoginTemplate from "../templates/LoginTemplate";
 
 export default function LoginPage() {
   const [keyRoom, setKeyRoom] = useState("");
   const navigate = useNavigate();
+  const socket = useSocket();
 
   const handleLogin = () => {
+    if (keyRoom === "") {
+      alert("Ingresar clave de la sala");
+      return;
+    }
+
+    socket.emit("join-room", {
+      idsoketUser: "User",
+      IdSocketReceiver: keyRoom,
+    });
+
     localStorage.setItem("keyRoom", keyRoom);
     navigate("/chat");
   };
