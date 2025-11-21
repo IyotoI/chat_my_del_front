4e044a7e3ef882b7e9e51a1d7551c469c18f7448
Santiago Yate <santiyate7@gmail.com>
2025-11-21 16:27:48 -0500

    Set a limit on the growth of chat message cards

--- src/components/molecules/CardMessages.jsx
+++ src/components/molecules/CardMessages.jsx
@@ -1,7 +1,7 @@
 export default function CardMessages({ className, value }) {
   return (
     <div
-      className={`${className} rounded-md break-all p-2 text-base/5 inline-block text-black`}
+      className={`${className} rounded-md break-all p-2 text-base/5 inline-block text-black max-w-[85%]`}
     >
       <div>{value.message}</div>
       <div className="text-end text-[#A2A2A2]">9:27 p.m.</div>
