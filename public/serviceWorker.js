c62df7cf45b13e5ce5a7f991268e5d8d81d70349
Santiago Yate <santiyate7@gmail.com>
2025-12-02 00:17:17 -0500

    When you press the push notification, redirect to the login page

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,8 +1,39 @@
 self.addEventListener("push", (e) => {
   const data = e.data.json();
-  console.log("🚀 ~ data:", data);
-  self.registration.showNotification(data.title, {
+
+  const options = {
     body: data.message,
-    icon: "",
-  });
+    data: {
+      url: data.url,
+    },
+  };
+
+  e.waitUntil(self.registration.showNotification(data.title, options));
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  const urlToOpen = new URL(event.notification.data.url, self.location.origin)
+    .href;
+
+  event.waitUntil(
+    clients
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then((clientList) => {
+        // Si ya hay una pestaña abierta → enfócarla y navegar
+        for (const client of clientList) {
+          if ("focus" in client) {
+            client.focus();
+            client.navigate(urlToOpen); // navegar dentro de la app
+            return;
+          }
+        }
+
+        // Si no hay ventana abierta → abrir una nueva
+        if (clients.openWindow) {
+          return clients.openWindow(urlToOpen);
+        }
+      })
+  );
 });
