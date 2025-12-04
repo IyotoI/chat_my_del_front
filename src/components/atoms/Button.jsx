f305cd3b4069086e306a6f336da137d70586faf7
Santiago Yate <santiyate7@gmail.com>
2025-12-04 02:57:44 -0500

    Improve the button so that it lasts longer

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -9,7 +9,7 @@ export default function Button({
     <button
       type={type}
       onClick={onClick}
-      className={`${className} ${color} text-white font-semibold py-3 px-8 rounded-full transition cursor-pointer`}
+      className={`${className} ${color} text-white font-semibold py-3 px-8 rounded-full transition cursor-pointer hover:bg-white   shadow-[0px_3px_8px_-4px_#777777] active:text-white hover:text-[#1AAD5E] active:bg-[#1AAD5E] active:shadow-none`}
     >
       {children}
     </button>
