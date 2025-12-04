f131b068267d54a99feccb3d6e57412f8538c93d
Santiago Yate <santiyate7@gmail.com>
2025-12-04 07:17:25 -0500

    test 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <div class="grid grid-rows-3 h-screen gap-4">
+    <div
+      class="grid grid-rows-3 h-screen gap-4"
+      style={{ height: "calc(0px + 100vh)" }}
+    >
       <div class="bg-red-300 flex-1">Espacio 1</div>
       <div class="bg-green-300 flex-1">Espacio 2</div>
       <div class="bg-blue-300 flex-1">
