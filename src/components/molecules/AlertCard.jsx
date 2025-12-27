d0d481255a387eec517162f9c781935fe8163a6e
Santiago Yate <santiyate7@gmail.com>
2025-12-26 19:47:23 -0500

    Test

--- src/components/molecules/AlertCard.jsx
+++ src/components/molecules/AlertCard.jsx
@@ -1,7 +1,18 @@
 import Button from "../atoms/Button";
 import IconItem from "../atoms/Icon";
+import { useGlobal } from "../../context/GlobalContext";
 
 const AlertCard = ({ titulo, description }) => {
+  const { setInitialState } = useGlobal();
+
+  const closeModal = () => {
+    setInitialState({
+      type: "SET_INITIAL_STATE",
+      key: "modalGeneral",
+      payload: { isOpenModal: false, nameComponentContent: "alertCard" },
+    });
+  };
+
   return (
     <div class="flex flex-col items-center">
       <IconItem color={"#1AAD5E"} size={3} className={"mb-8"} />
@@ -11,7 +22,9 @@ const AlertCard = ({ titulo, description }) => {
       <h4 class="font-semibold text-lg flex items-center gap-2 text-gray-500">
         Desripcion
       </h4>
-      <Button color="bg-[#1AAD5E] mt-8">Entiendo</Button>
+      <Button color="bg-[#1AAD5E] mt-8" onClick={closeModal}>
+        Entiendo
+      </Button>
     </div>
   );
 };
--- src/context/appReducer.js
+++ src/context/appReducer.js
@@ -5,7 +5,7 @@ export const initialState = {
   listItemsContacts: [],
   modalGeneral: {
     isOpenModal: false,
-    nameComponentContent: "alertCard",
+    nameComponentContent: "",
   },
 };
 
